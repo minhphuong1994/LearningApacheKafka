37d181f4ca2e35530af8ddb4bf0922d34f4c132d
PhuongLy <phuong.ly.k2set@gmail.com>
2024-11-05 12:10:43 +0700

    Practice-00: inititalize Kafka learning project with SpringBoot

---
+++ cab-driver/src/test/java/com/phuongly/cab_driver/CabDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuongly.cab_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cab-user/src/test/java/com/phuongly/cab_user/CabUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phuongly.cab_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
