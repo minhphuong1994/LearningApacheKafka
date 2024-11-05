bc4477ee434bcdd5c3ab4cc7165a578c1152c128
PhuongLy <phuong.ly.k2set@gmail.com>
2024-11-05 22:56:11 +0700

    Practice-03: implement listener/receiver in cab-user app

--- cab-user/src/main/java/com/phuongly/cab_user/service/LocationService.java
+++ cab-user/src/main/java/com/phuongly/cab_user/service/LocationService.java
@@ -1,4 +1,13 @@
 package com.phuongly.cab_user.service;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LocationService {
+
+    @KafkaListener(topics = "cab-location",groupId = "user-group")
+    public void cabLocation(String location){
+        System.out.println(location);
+    }
 }
